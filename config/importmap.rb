cecdfe078f543b44770ac65eb01d2eac18e4caee
rifrifqyqy <rifqyh22@gmail.com>
2024-10-03 19:19:32 +0700

    create routes for root and about pages

---
+++ config/importmap.rb
@@ -0,0 +1,4 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "vue", to: "https://unpkg.com/vue@3.2.20/dist/vue.esm-browser.js"
---
+++ config/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html", "./app/helpers/**/*.rb", "./app/javascript/**/*.js", "./app/views/**/*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
